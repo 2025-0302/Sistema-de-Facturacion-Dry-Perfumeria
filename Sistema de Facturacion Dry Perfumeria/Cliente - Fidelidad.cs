31d7baf999cb9e05648f95057096370d6b6a1745
yendr <yendr@LPYENDRY>
2026-05-30 19:37:16 -0400

    Finalizando 80%

---
+++ Sistema de Facturacion Dry Perfumeria/Cliente - Fidelidad.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_de_Facturacion_Dry_Perfumeria
+{
+    public partial class Cliente___Fidelidad : Form
+    {
+        public Cliente___Fidelidad()
+        {
+            InitializeComponent();
+        }
+    }
+}
